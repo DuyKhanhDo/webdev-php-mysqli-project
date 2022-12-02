491a1f2f4b65953b193c035d16bb0974b2acda33
DuyKhanhDo <116875186+DuyKhanhDo@users.noreply.github.com>
2022-12-02 10:15:17 +0000

    small update for date

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,7 @@
- <div class="footerContainer">
+<?php
+$year = date("Y");
+?>
+<div class="footerContainer">
 <footer>
       <nav>
         <menu>
@@ -8,6 +11,7 @@
           <li><a href="#">Twitter</a></li>
         </menu>
       </nav>
-      <div>&copy;</div>
+      <div>&copy;<?php echo $year;?>
+      </div>
     </footer>
 </div>
